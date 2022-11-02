71494638526641d21ca1239519fa22eaa185c70f
unknown <outhmaneouykhf11@gmail.com>
2022-11-02 11:17:10 +0100

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request')
+
+const forecast = (longitude, latitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f146394cd1b44461c1f9d85f6cdec135&query='+ longitude +',' + latitude
+
+    request({ url, json: true}, (error, {body}) => {
+        if (error) {
+            callback('Unable to connect to weather services !', undefined)
+        } else if (body.error) {
+            callback('Unable to find location !', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '.It is ' + body.current.temperature + ' degrees out, it feels like '+ body.current.feelslike + ' degrees out.'
+            )
+        }
+    })
+}
+
+module.exports = forecast
